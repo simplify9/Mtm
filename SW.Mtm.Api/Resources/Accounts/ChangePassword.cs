ff8d57ac0e2d04a078efdd25d5877e3426a9c59b
Samer Awajan <samer@simplify9.com>
2020-09-15 13:01:02 +0300

    ..

---
+++ SW.Mtm.Api/Resources/Accounts/ChangePassword.cs
@@ -0,0 +1,66 @@
+﻿using FluentValidation;
+using SW.Mtm.Domain;
+using SW.Mtm.Model;
+using SW.PrimitiveTypes;
+using System.Threading.Tasks;
+
+namespace SW.Mtm.Resources.Accounts
+{
+    [Protect]
+    class ChangePassword : ICommandHandler<string, AccountChangePassword>
+    {
+        private readonly RequestContext requestContext;
+        private readonly MtmDbContext dbContext;
+
+        public ChangePassword(RequestContext requestContext, MtmDbContext dbContext)
+        {
+            this.requestContext = requestContext;
+            this.dbContext = dbContext;
+        }
+
+        async public Task<object> Handle(string key, AccountChangePassword request)
+        {
+
+            var accountId = requestContext.GetNameIdentifier();
+
+            if (accountId != key)
+                throw new SWUnauthorizedException();
+
+
+
+
+            //
+            var account = await dbContext.FindAsync<Account>(key);
+            if (account == null)
+                throw new SWNotFoundException(key);
+
+            if (account.EmailProvider != EmailProvider.None)
+                throw new SWValidationException("EmailProvider", "Not allowed for external email providers.");
+
+            if (account.LoginMethods & LoginMethod.EmailAndPassword)
+
+
+
+            var passwordCheck = SecurePasswordHasher.Verify(request.CurrentPassword, account.Password);
+
+            if (!passwordCheck)
+                throw new SWUnauthorizedException();
+
+            account.SetPassword(SecurePasswordHasher.Hash(request.NewPassword));
+
+            await dbContext.SaveChangesAsync();
+
+            return null;
+
+        }
+
+        class Validate : AbstractValidator<AccountChangePassword>
+        {
+            public Validate()
+            {
+                RuleFor(i => i.CurrentPassword).NotEmpty();
+                RuleFor(i => i.NewPassword).NotEmpty();
+            }
+        }
+    }
+}
---
+++ SW.Mtm.Sdk/Model/AccountChangePassword.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SW.Mtm.Model
+{
+    public class AccountChangePassword
+    {
+        public string CurrentPassword { get; set; }
+        public string NewPassword { get; set; }
+    }
+}
