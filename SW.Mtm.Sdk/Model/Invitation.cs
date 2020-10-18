5ce554fc037b48b91116544dcb499d0b29d01562
Samer Awajan <samer@simplify9.com>
2020-10-18 17:11:08 +0300

    ..

---
+++ SW.Mtm.Sdk/Model/Invitation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SW.Mtm.Model
+{
+    public class InvitationAccept
+    {
+    }
+
+    //public class InvitationAcceptResult
+    //{
+    //    public string AccountId { get; set; }
+    //    public string MyProperty { get; set; }
+    //}
+}
